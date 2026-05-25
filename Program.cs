9bdb82dd4cfaf0c923ccf9c14c57de38b73f35cd
Tauhirh <ST10489361@rcconnect.edu.za>
2026-05-25 18:45:38 +0200

    Initial commit: Project structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CybersecurityChatbotGUI
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Cybersecurity Chatbot");
+        }
+    }
+}
